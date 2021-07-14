2f17ff4fd3da12ced15c6fb6908ce677b1bcaf2d
JoseBorbon <joserborbon@gmail.com>
2021-07-14 11:59:31 -0400

    Hotfixes to boardStateJS draw and Game constructor

--- boardState.js
+++ boardState.js
@@ -1,8 +1,9 @@
 //add exports at the end whenever
 //create shuffle function
-function draw(hand, deck, n) {//should be usable for most draw cards as well
-    for (let i = 0, i<n, i++) {
-        hand.push(deck.pop())
+function draw(hand, deck, numOfCardsToDraw) {//should be usable for most draw cards as well
+    //guaranteed to at least draw 1 so decided to use 1 and <= numOfCardsToDraw
+    for (let i = 1; i <= numOfCardsToDraw; i++) {
+        hand.push(deck.pop());
     }
 }
 function displayBoard(hand, playerField, enemyField,fieldSpell) {
@@ -13,8 +14,8 @@ function displayBoard(hand, playerField, enemyField,fieldSpell) {
     ${playerField}\n
     \n
     Hand:\n
-    ${hand}`
-    return view
+    ${hand}`;
+    return view;
 }
 //probably want a function or some way to check card descriptions
 
@@ -28,44 +29,49 @@ class Game {
         //insert shuffle function
         this._hand1 = [],
         this._hand2 = [],
-        this._field1 = [[],[]],//[[monsters],[spells]]
+        this._field1 = [[],[]],//[[monsters],[spells / traps]]
         this._field2 = [[],[]],//add limitations for card types and sizes later
         this.fieldSpell = []
     }
-    //prob going to need an init line somewhere
-    //bunch of user input lines/functions for play/reactions
-    //
-    draw(this._hand1, this._deck1, 5)
-    draw(this._hand2, this._deck2, 5)
-    let i = 0;
-    while (this._lifePts1 > 0 && this._lifePts2 > 0) {//loop until someone's out of points
-        if (i%2 === 0) {//setting up turns
-            if (i === 0) {//skip draw on first turn
 
-            }
-            else {//draw
-                draw(this._hand1, this._deck1, 1)
-            }
-            //standby, check effects/ magic/trap; will likely prompt user input
-            //main1: probably maintaining an active display and a way to interact with 
-                //the cards and view details
-            //battle; will probably need other functions for the commands
-            if (i === 0) {//skip battle on first turn
 
+    //methods
+    itsTimeToDuel() {
+        //prob going to need an init line somewhere
+        //bunch of user input lines/functions for play/reactions
+        draw(this._hand1, this._deck1, 5)
+        draw(this._hand2, this._deck2, 5)
+        let i = 0;
+        while (this._lifePts1 > 0 && this._lifePts2 > 0) {//loop until someone's out of points
+            if (i%2 === 0) {//setting up turns
+                if (i === 0) {//skip draw on first turn
+    
+                }
+                else {//draw
+                    draw(this._hand1, this._deck1, 1)
+                }
+                //standby, check effects/ magic/trap; will likely prompt user input
+                //main1: probably maintaining an active display and a way to interact with 
+                    //the cards and view details
+                //battle; will probably need other functions for the commands
+                if (i === 0) {//skip battle on first turn
+    
+                }
+                //main2
+                //end
             }
-            //main2
-            //end
+            else {//player 2 turns
+                //draw
+                draw(this._hand2, this._deck2, 1)
+                //standby, check effects/ magic/trap; will likely prompt user input
+                //main1
+                //battle
+                //main2
+                //end
+            }
+    
+            i++
         }
-        else {//player 2 turns
-            //draw
-            draw(this._hand2, this._deck2, 1)
-            //standby, check effects/ magic/trap; will likely prompt user input
-            //main1
-            //battle
-            //main2
-            //end
-        }
-
-        i++
     }
+    
 }
