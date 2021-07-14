5a3637587c8763a812a4c4937e51aa269e750eb8
BennyZ3 <54120872+BennyZ3@users.noreply.github.com>
2021-07-14 10:50:39 -0400

    modified draw function to be more universal, commented for general turn layout

--- boardState.js
+++ boardState.js
@@ -1,10 +1,12 @@
+//add exports at the end whenever
 //create shuffle function
-function startingDraw(hand, deck) {
-    for (let i = 0, i<7,i++) {
+function draw(hand, deck, n) {//should be usable for most draw cards as well
+    for (let i = 0, i<n, i++) {
         hand.push(deck.pop())
     }
 }
 function display(hand, playerField, enemyField,fieldSpell) {
+    //can prob be ran inside a log function if it's to be played in a terminal
     let view = `Field State:\n
     ${enemyField}\n
     ${fieldSpell}\n
@@ -20,6 +22,8 @@ class Game {
     constructor(user1, user2) {
         this._deck1 = user1.onHandDeck,
         this._deck2 = user2.onHandDeck,
+        this._lifePts1 = user1.lifePoints,
+        this._lifePts2 = user2.lifePoints,
         //insert fusion/ritual deck as needed
         //insert shuffle function
         this._hand1 = [],
@@ -30,5 +34,38 @@ class Game {
     }
     //prob going to need an init line somewhere
     //bunch of user input lines/functions for play/reactions
+    //
+    draw(this._hand1, this._deck1, 5)
+    draw(this._hand2, this._deck2, 5)
+    let i = 0;
+    while (this._lifePts1 > 0 && this._lifePts2 > 0) {//loop until someone's out of points
+        if (i%2 === 0) {//setting up turns
+            if (i === 0) {//skip draw on first turn
 
+            }
+            else {//draw
+                draw(this._hand1, this._deck1, 1)
+            }
+            //standby, check effects/ magic/trap; will likely prompt user input
+            //main1: probably maintaining an active display and a way to interact with 
+                //the cards and view details
+            //battle; will probably need other functions for the commands
+            if (i === 0) {//skip battle on first turn
+
+            }
+            //main2
+            //end
+        }
+        else {//player 2 turns
+            //draw
+            draw(this._hand2, this._deck2, 1)
+            //standby, check effects/ magic/trap; will likely prompt user input
+            //main1
+            //battle
+            //main2
+            //end
+        }
+
+        i++
+    }
 }
